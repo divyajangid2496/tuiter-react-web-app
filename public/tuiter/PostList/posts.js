5578b32fe7279dbd5cffc986d504c9d198263729
divyajangid2496 <92000178+divyajangid2496@users.noreply.github.com>
2023-02-20 17:43:19 -0500

    Home page

---
+++ public/tuiter/PostList/posts.js
@@ -0,0 +1,35 @@
+let posts;
+export default posts = [
+    {
+        userAvatar: "../images/tesla.webp",
+        userName: "Elon Musk",
+        userHandle: "elonmusk",
+        time: "23h",
+        title: "Amazing show about @Inspiration4x mission!",
+        markText: "@Inspiration4x",
+        postImage: "../images/post1.webp",
+        postTitle: "Countdown: Inspiration4x Mission to Space | Netflix Office Site",
+        postContent: "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 " +
+            "crew on the first all-civilian orbital space...",
+        postLinkName: "netflix.com",
+        postLink: "https://www.netflix.com/",
+        reply: "4.2K",
+        retweet: "3.5K",
+        like: "37.5K",
+    },
+    {
+        userAvatar: "../images/nyposts.png",
+        userName: "New York Post",
+        userHandle: "nypost",
+        time: "23h",
+        title: "Grimes seen reading Karl Marx following split with world's richest man Elon Musk trib.al/nx2Gfaq",
+        markText: "trib.al/nx2Gfaq",
+        postImage: "../images/post2.webp",
+        postTitle: "",
+        postContent: "",
+        postLink: "",
+        reply: "703",
+        retweet: "1,689",
+        like: "3,764",
+    }
+]
